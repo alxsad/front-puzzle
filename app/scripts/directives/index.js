6509318db548e2dfc39d37116394c93a4dabe4ec
Alex Davidovich <alxsad@gmail.com>
2014-06-18 16:40:45 +0300

    add readme file

--- app/scripts/directives/index.js
+++ app/scripts/directives/index.js
@@ -5,17 +5,16 @@ var app = angular.module('app');
 app.directive('activeLink', function ($location) {
     return {
         restrict: 'A',
-        link: function (scope, element, attrs) {
+        link: function (scope, element) {
             scope.$watch(
                 function () {
                     return $location.path();
                 },
                 function (newPath) {
-                    console.log(newPath);
                     element.find('li').removeClass('active');
                     angular.forEach(element.find('a'), function (value) {
                         var a = angular.element(value);
-                        if (a.attr('href').substr(1) == $location.path().substr(1)) {
+                        if (a.attr('href').substr(1) == newPath.substr(1)) {
                             a.parents('li').addClass('active');
                         }
                     });
